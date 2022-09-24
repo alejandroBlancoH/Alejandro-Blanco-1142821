c2705c946f42ed2a97896d961c235174a69fc3e2
alejandroBlancoH <113808730+alejandroBlancoH@users.noreply.github.com>
2022-09-24 12:01:24 -0600

    lab

---
+++ Semana 7/L1_ABH_1142821/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace L1_ABH_1142821
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese su nombre: ");
+            string nombre=Console.ReadLine();
+
+            
+            Console.WriteLine("Hola mundo ");
+            Console.WriteLine("Soy " + nombre );
+
+            Console.Write("Hola mundo");
+            Console.Write(" soy " + nombre);
+            Console.ReadKey(); 
+
+        }
+    }
+}
