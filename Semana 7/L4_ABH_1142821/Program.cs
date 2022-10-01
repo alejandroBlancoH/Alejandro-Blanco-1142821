fa09e065278e5e0cac1f37f6935c02f831bd3eac
alejandroBlancoH <113808730+alejandroBlancoH@users.noreply.github.com>
2022-09-30 19:08:42 -0600

    Tareas de semana 7

---
+++ Semana 7/L4_ABH_1142821/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4_ABH_1142821
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables e intro
+            Console.WriteLine("Ejercicio 1: operaciones aritméticas");
+            Console.WriteLine("Ingrese dos números");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            //Procesos
+            double a = x + y;
+            Console.WriteLine( x + " + " + y + " = " + a);
+            double b = x - y;
+            Console.WriteLine(x + " - " + y + " = " + b);
+            double c = x / y;
+            Console.WriteLine(x + " ÷ " + y + " = " + c);
+
+            Console.ReadKey();
+             
+        }
+    }
+}
---
+++ Semana 7/T1_ABH_1142821/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1_ABH_1142821
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi segundo programa");
+            string SNombre, SEdad, SCarrera, Scarne = "";
+            Console.WriteLine("Nombre:");
+            SNombre = Console.ReadLine();
+            Console.WriteLine("Edad:");
+            SEdad = Console.ReadLine();
+            Console.WriteLine("Carrera:");
+            SCarrera = Console.ReadLine();
+            Console.WriteLine("Carné:");
+            Scarne = Console.ReadLine();
+
+            Console.WriteLine("Soy "+ SNombre + " ,tengo " + SEdad + " años y estudio la carrera de " + SCarrera + " . Mi número de carné es; "+ Scarne );
+            Console.ReadKey();
+        }
+    }
+}
