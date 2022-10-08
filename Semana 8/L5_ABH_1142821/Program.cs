8da8c3c3572c9b201d0274b90678f199f6c94b39
alejandroBlancoH <113808730+alejandroBlancoH@users.noreply.github.com>
2022-10-07 18:34:26 -0600

    semana 8

---
+++ Semana 8/L5_ABH_1142821/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace L5_ABH_1142821
+{
+    class Program
+    {
+        static void ejercicio1()
+        {
+            Console.WriteLine("Ejercicio 1");
+            Console.Write(" Numero entero: ");
+            try
+            {
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                if (x > 0)
+                {
+                    Console.WriteLine("Es positivo");
+                }
+                else if (x == 0)
+                {
+                    Console.WriteLine("Es cero");
+                }
+                else
+                {
+                    Console.WriteLine("Es negativo");
+                }
+                Console.ReadKey();
+            }
+
+            catch (FormatException exception)
+            {
+                Console.WriteLine("No es un número");
+                Console.WriteLine(exception.Message);
+
+            }
+
+            Console.ReadKey();
+        }
+        static void ejercicio2()
+        {
+            Console.WriteLine("Ingrese un número");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(" Serie de Fibonacci: ");
+            int a = 0, b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a + ",");
+                int aux = a;
+                a = b;
+                b = aux + b;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int opcion;
+            do
+            {
+                Console.WriteLine("Ingrese un número de ejercicio: ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                ejercicio1();
+                break;
+                    case 2:
+                        ejercicio2();
+                        break;
+                    case 0:
+                        Console.WriteLine("Adiós!");
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida");
+                        break;
+                }
+            } while (opcion != 0);
+        }
+    }
+}
---
+++ Semana 8/T7_ABH_1142821/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T7_ABH_1142821
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double suma=0;
+            double suma2 = 0;
+            double suma3 = 0;
+            menu:
+            Console.WriteLine("Ingrese un número mayor a 0");
+            double n = Convert.ToDouble(Console.ReadLine());
+
+            if (n>0)
+            {
+                Console.WriteLine("Fórmula 1:");
+                for (double i=1; i<=n; i++)
+                {
+                    suma = suma +(1 / i);
+                }
+               Console.WriteLine(Math.Round(suma,2));
+
+                Console.WriteLine("Fórmula 2:");
+                for (double a=1; a <=n ;a++)
+                {
+                    suma2 = suma2 + (1 / Math.Pow(2, a));
+                }
+                Console.WriteLine(Math.Round(suma2, 4));
+
+                for (double a=1; a<=n; a++)
+                {
+                    porsi:
+                    Console.WriteLine("Ingrese dos valores para realizar el tercer proceso");
+                    double x= Convert.ToDouble(Console.ReadLine());
+                    double b= Convert.ToDouble(Console.ReadLine());
+                    if ((x>0) && (b > 0))
+                    {
+                        double resta1 = n - a;
+                        suma3 = suma3 +(Math.Pow(x, a)) *(Math.Pow(b,resta1 )) ; 
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Error");
+                        goto porsi;
+                    }
+
+                }
+                Console.WriteLine(Math.Round(suma3, 4));
+
+            }
+            else
+            {
+                Console.WriteLine("Error. Vuelva a intentar");
+                goto menu;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
