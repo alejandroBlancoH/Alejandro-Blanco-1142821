145e5db85e94997e1260176494354510962b593b
alejandroBlancoH <113808730+alejandroBlancoH@users.noreply.github.com>
2022-10-14 20:15:15 -0600

    semana 9

---
+++ Semana 9/Lab10_ABH_1142821/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10_ABH_1142821
+{
+    class Circulo
+    {
+        private double radio;
+
+        public Circulo(double radio)
+        {
+            
+        }
+        
+        private double ObtenerPerimetro()
+        {
+            double p = (2 * radio) * (3.1416);
+            return p;
+        }
+
+        private double ObtenerArea()
+        {
+            double area = (3.1416)* radio;
+            return area;
+        }
+        private double ObtenerVolumen()
+        {
+            double volumen = (1.33)*(3.1416)*(radio)*(radio)*(radio);
+            return volumen;
+        }
+        public void calcular
+        {
+
+        }
+    }
+}
---
+++ Semana 9/Lab10_ABH_1142821/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab10_ABH_1142821
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
