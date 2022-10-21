30bb66d13006c81aff74aa3cb433f1b6a04b0bd0
alejandroBlancoH <113808730+alejandroBlancoH@users.noreply.github.com>
2022-10-21 14:19:41 -0600

    Semana 10

---
+++ Semana 10/Semana10_ABH/Circulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Semana10_ABH
+{
+    class Circulo
+    {
+        public double radio;
+
+        public double ObtenerPerimetro()
+        {
+            return 2 * Math.PI * radio;
+        }
+        public double ObtenerArea()
+        {
+            return Math.PI * (radio * radio);
+        }
+        public double ObtenerVolumen()
+        {
+            return 4 * Math.PI * radio * radio * radio / 3;
+        }
+
+    }
+}
---
+++ Semana 10/Semana10_ABH/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Semana10_ABH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo circulo = new Circulo();
+            Console.WriteLine("Ingrese un radio");
+            circulo.radio = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Perimetro: " + circulo.ObtenerPerimetro ());
+            Console.WriteLine("Área: " + circulo.ObtenerArea ());
+            Console.WriteLine("Volumen: " + circulo.ObtenerVolumen ());
+        }
+    }
+}
